34efaeb261f7b852032878d04b763072207fbd4e
unknown <tranquangthanh191@gmail.com>
2020-04-02 13:56:22 +0800

    update main

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 import database as db
+import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='static/build')
+cors = CORS(app, resources={r"/*": {"origins": "*", "supports_credentials": True}})
 
 
-@app.route('/')
-def main():
-    connection, cursor = db.init()
-    return ''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.json
+        print(user)
+        return json.dumps({'status': 'valid'})
 
 
 if __name__ == '__main__':
-    app.run()
+    connection, cursor = db.init()
+    app.run(host='0.0.0.0', port=5000, debug=True)
