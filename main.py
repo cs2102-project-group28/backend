7cd54c8a7417f1408c257a03f310cebeb294fa7e
unknown <tranquangthanh191@gmail.com>
2020-05-07 15:34:31 +0800

    add response to return

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, json
 from flask_cors import CORS
 import database as db
 import user_query as uqr
@@ -21,8 +21,8 @@ def login():
         password = user['password']
         pos = uqr.login(cursor, username, password)
         if pos is None:
-            return {'message': 'Username or password is incorrect'}, 400
-        return pos, 200
+            return Response(response=json.dumps({'message': 'Username or password is incorrect'}), status=400)
+        return Response(response=json.dumps(pos), status=200)
 
 
 @app.route('/register', methods=['POST'])
@@ -35,7 +35,7 @@ def register():
         user_type = new_user['userType']
         rider_type = new_user['riderType']
         if new_user['username'] == '' or new_user['password'] == '' or new_user['userType'] == '':
-            return {'message': 'Some fields are empty'}, 400
+            return Response(response=json.dumps({'message': 'Some fields are empty'}), status=400)
         uqr.register(connection, cursor, username, password, phone, user_type, rider_type)
         return Response(status=200)
 
@@ -45,7 +45,7 @@ def update(username):
     if request.method == 'POST':
         new_user = request.json
         if 'password' not in new_user and 'phone' not in new_user:
-            return {'message': 'All fields are empty'}, 400
+            return Response(response=json.dumps({'message': 'All fields are empty'}), status=400)
         elif 'phone' not in new_user:
             uqr.update(connection, cursor, username, password=new_user['password'])
         elif 'password' not in new_user:
@@ -62,7 +62,8 @@ def view_profile(username):
         phone = int(user['phone'])
         user_type = user['userType']
         reward_points = int(user['rewardPoints'])
-        return {'data': uqr.get_profile(cursor, username, phone, user_type, reward_points)}, 200
+        return Response(response=json.dumps({'data': uqr.get_profile(cursor, username, phone, user_type, reward_points)}),
+                        status=200)
 
 
 @app.route('/<username>/update/customer', methods=['POST'])
@@ -87,7 +88,8 @@ def view_menu(username):
         location = tuple(menu['location'])
         fName = tuple(menu['fName'])
         fCategory = tuple(menu['fCategory'])
-        return {'data': mqr.get_menu(cursor, rName, rCategory, location, fName, fCategory)}, 200
+        return Response(response=json.dumps({'data': mqr.get_menu(cursor, rName, rCategory, location, fName, fCategory)}),
+                        status=200)
 
 
 @app.route('/customer/<username>/order/checkout/<rid>/<list:fid>/<list:quantity>/promo', methods=['POST'])
@@ -100,33 +102,35 @@ def checkout(username, rid, fid, quantity):
             try:
                 uqr.verify_customer(cursor, username, creditcard, cvv)
             except Exception:
-                return {'message': 'Credit card or cvv is not correct'}, 400
+                return Response(response=json.dumps({'message': 'Credit card or cvv is not correct'}), status=400)
         try:
             ptype = request.args.get('ptype')
             pid = request.args.get('pid')
-            return mqr.checkout(connection, cursor, username, rid, fid, quantity, ptype, pid, customer['location']), 200
+            return \
+                Response(response=json.dumps(mqr.checkout(connection, cursor, username, rid, fid, quantity, ptype, pid, customer['location'])),
+                         status=200)
         except Exception as e:
-            return {'message': str(e)}, 400
+            return Response(response=json.dumps({'message': str(e)}), status=400)
 
 
 @app.route('/customer/<username>/view-promotion', methods=['POST'])
 def view_promotion(username):
     if request.method == 'POST':
-        return {'data': pqr.view_promotion(cursor)}, 200
+        return Response(response=json.dumps({'data': pqr.view_promotion(cursor)}), status=200)
 
 
 @app.route('/customer/<username>/past-order/date', methods=['POST'])
 def past_order(username):
     startdate = request.args.get('startdate')
     enddate = request.args.get('enddate')
-    return {'data': mqr.view_past_order(cursor, username, startdate, enddate)}, 200
+    return Response(response=json.dumps({'data': mqr.view_past_order(cursor, username, startdate, enddate)}), status=200)
 
 
 @app.route('/customer/<username>/search-food/<item>', methods=['POST'])
 def search_food(username, item):
     if request.method == 'POST':
         item = str.lower(item)
-        return {'data': mqr.get_food(cursor, item)}, 200
+        return Response(response=json.dumps({'data': mqr.get_food(cursor, item)}), status=200)
 
 
 @app.route('/customer/<username>/post-review/<oid>', methods=['POST'])
@@ -141,7 +145,7 @@ def review_order(username, oid):
 @app.route('/customer/<username>/view-review/<rid>', methods=['POST'])
 def view_review(username, rid):
     if request.method == 'POST':
-        return {"data": mqr.view_review(cursor, rid)}, 200
+        return Response(response=json.dumps({"data": mqr.view_review(cursor, rid)}), status=200)
 
 
 @app.route('/customer/<username>/rate-deliver/<oid>/<rating>', methods=['POST'])
@@ -155,14 +159,14 @@ def rate_deliver(username, oid, rating):
 def recent_locations(username):
     if request.method == 'POST':
         locations = mqr.get_recent_location(cursor, username)
-        return {"data": locations}, 200
+        return Response(response=json.dumps({"data": locations}), status=200)
 
 
 @app.route('/customer/<username>/search-restaurant/<restaurant>', methods=['POST'])
 def search_restaurants(username, restaurant):
     if request.method == 'POST':
         restaurant = str.lower(restaurant)
-        return {'data': mqr.get_restaurant(cursor, restaurant)}, 200
+        return Response(response=json.dumps({'data': mqr.get_restaurant(cursor, restaurant)}), status=200)
 
 
 @app.route('/staff/<username>/summary/promotion/<startdate>/<enddate>', methods=['POST'])
@@ -171,8 +175,8 @@ def summary_promotion(username, startdate, enddate=None):
     if request.method == 'POST':
         data = pqr.summary(cursor, startdate, enddate, username)
         if len(data) == 0:
-            return {'message': 'No promotions within this time for your restaurant'}, 400
-        return {'data': data}, 200
+            return Response(response=json.dumps({'message': 'No promotions within this time for your restaurant'}), status=400)
+        return Response(response=json.dumps({'data': data}), status=200)
 
 
 @app.route('/staff/<username>/add-food/<fName>/<fCategory>', methods=['POST'])
@@ -199,34 +203,34 @@ def update_menu(username):
 @app.route('/staff/<username>/summary/order/<month>/<year>', methods=['POST'])
 def view_monthly_order(username, month, year):
     if request.method == 'POST':
-        return mqr.view_monthly_order(cursor, username, month, year), 200
+        return Response(response=json.dumps(mqr.view_monthly_order(cursor, username, month, year)), status=200)
 
 
 @app.route('/manager/<username>/month-summary/<month>/<year>', methods=['POST'])
 def month_summary(username, month, year):
     if request.method == 'POST':
-        return mngqr.month_summary(cursor, month, year), 200
+        return Response(response=json.dumps(mngqr.month_summary(cursor, month, year)), status=200)
 
 
 @app.route('/manager/<username>/order/<area>/<day>/<starttime>/<endtime>', methods=['POST'])
 def order_summary(username, area, day, starttime, endtime):
     if request.method == 'POST':
         data = mngqr.order_summary(cursor, area, day, starttime, endtime)
-        return data, 200
+        return Response(response=json.dumps(data), status=200)
 
 
 @app.route('/manager/<username>/rider/<month>/<year>', methods=['POST'])
 def rider_summary(username, month, year):
     if request.method == 'POST':
         data = mngqr.rider_summary(cursor, month, year)
-        return {'data': data}, 200
+        return Response(response=json.dumps({'data': data}), status=200)
 
 
 @app.route('/rider/<username>/summary/<month>/<year>', methods=['POST'])
 def summary(username, month, year):
     if request.method == 'POST':
         data = rqr.summary(cursor, username, month, year)
-        return data, 200
+        return Response(response=json.dumps(data), status=200)
 
 
 @app.route('/rider/<username>/set-deliver-depart/<oid>', methods=['POST'])
@@ -250,7 +254,7 @@ def add_schedule_part_time(username, sid):
             rqr.add_schedule_part_time(username, sid)
             return Response(status=200)
         except Exception as e:
-            return {"message": str(e)}, 400
+            return Response(response=json.dumps({"message": str(e)}), status=400)
 
 
 @app.route('/rider/part-time/<username>/delete-schedule/<list:sid>', methods=['POST'])
@@ -260,7 +264,7 @@ def delete_schedule(username, sid):
             rqr.delete_schedule_part_time(connection, cursor, username, sid)
             return Response(status=200)
         except Exception as e:
-            return {"message": str(e)}, 400
+            return Response(response=json.dumps({"message": str(e)}), status=400)
 
 
 @app.route('/rider/full-time/<username>/add-schedule/<startday>/<endday>/<shift>', methods=['POST'])
